9fdb361fd8c80fc66d87156ffc428fe3285ec082
prodhanm <mustafizur.prodhan@gmail.com>
2025-04-27 13:10:52 -0400

    modified .gitignore

---
+++ Linear.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class Linear {
+    public static int search(int[] list, int num, int key) {
+        for(int i=0; i<num; i++) {
+            if(list[i] == key) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int num;
+        int key;
+        int list[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+        System.out.print("Enter the number of an elemen: ");
+        num = inp.nextInt(); 
+        int index = search(list, num, key=40);
+        if(index == -1) {
+            System.out.println("Element not found in the list.");
+        } else {
+            System.out.println("Element found at index: " + index);
+        }
+        inp.close();
+    }
+}
---
+++ linear.py
@@ -0,0 +1,18 @@
+def search(li, num, key):
+    for num in range(len(li)):
+        if li[num] == key:
+            return num
+    return -1
+        
+def main():
+    li = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+    num = int(input("Enter a number of an element: "))
+    key = 50
+    ind = search(li, num, key)
+    if ind == -1:
+        print("Element not found")
+    else:
+        print(f"Element found at index {ind}")
+
+if __name__ == "__main__":
+    main()

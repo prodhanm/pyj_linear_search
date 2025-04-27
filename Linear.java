93cb4ee4db7d250d868a5b7c3bc364bd5825f1b9
prodhanm <mustafizur.prodhan@gmail.com>
2025-04-27 13:23:11 -0400

    slight modification to the java file

--- Linear.java
+++ Linear.java
@@ -15,7 +15,7 @@ class Linear {
         int num;
         int key = 40;
         int list[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
-        System.out.print("Enter the number of an elemen: ");
+        System.out.print("Enter the number of an element: ");
         num = inp.nextInt(); 
         int index = search(list, num, key);
         if(index == -1) {
